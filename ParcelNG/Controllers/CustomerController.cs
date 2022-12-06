2dcc3390f3f840f36e53ebc4e71c8d16cd6fa896
Rando <ogunsanyaakinyemi@gmail.com>
2022-12-06 03:59:45 +0100

    Add project files.

---
+++ ParcelNG/Controllers/CustomerController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using ParcelNG.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ParcelNG.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET: api/<CustomerController>
+
+
+        private SqlConnection _conn;
+        private SqlDataAdapter _adapter;
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            _conn = new SqlConnection("data source=LAPTOP-PB27B2LT\\SQLEXPRESS;;Initial catalog=ParcelMessenger;user id=sa;password=Password123#;");
+            DataTable dt = new DataTable();
+            var query = "SELECT * FROM [ParcelMessenger].[dbo].[Customer]";
+            _adapter = new SqlDataAdapter
+            {
+                SelectCommand = new SqlCommand(query, _conn)
+            };
+            _adapter.Fill(dt);
+            List<Customer> customers = new List<Customer>(dt.Rows.Count);
+            if (dt.Rows.Count > 0)
+            {
+                foreach(DataRow customerRecord in dt.Rows)
+                {
+                    customers.Add(new readCustomer(customerRecord));
+                }
+            }
+
+            return customers;
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ParcelNG/Model/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParcelNG.Model
+{
+    public class Customer
+    {   [Key]
+        public int CustomerID { get; set; }
+        [Required]
+        public String CustomerNumber { get; set; }
+        public String Title { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String PhoneNumber { get; set; }
+        public String Email { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+    public class createCustomer : Customer
+    {
+
+    }
+    public class readCustomer : Customer
+    {
+        public readCustomer(DataRow row)
+        {
+
+            CustomerID = Convert.ToInt32(row["CustomerID"]);
+            CustomerNumber = row["CustomerNumber"].ToString();
+            Title = row["Title"].ToString();
+            FirstName = row["FirstName"].ToString();
+            LastName = row["LastName"].ToString();
+            PhoneNumber = row["PhoneNumber"].ToString();
+            Email = row["Email"].ToString();
+            if (!(row["UpdateDate"] is DBNull))
+            {
+                UpdateDate = Convert.ToDateTime(row["UpdateDate"]);
+            }
+            if (!(row["CreateDate"] is DBNull))
+            {
+                CreateDate = Convert.ToDateTime(row["CreateDate"]);
+            }
+        }
+        
+    }
+}
